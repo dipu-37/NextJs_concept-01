04cf54f0e510100902beae780e5e6a980e1d02cf
dipu-37 <razibul1105nstu@gmail.com>
2024-10-19 14:50:50 +0600

    cookies

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,16 @@
 import { NextResponse } from "next/server"
 
+const user = true;
 export const middleware = (request)=>{
-    return NextResponse.redirect(new URL('about',request.url))
+    const coo ='hdjkhjsha'
+    console.log( request.cookies.get('token'))
+    const cookies = request.cookies.get('token')
+    if (!cookies || cookies.value !== coo ){
+    return NextResponse.redirect(new URL('/login',request.url))}
+    return NextResponse.next()
 }
 
 
 export const config = {
-    matcher: '/profile'
+    matcher: ['/dashboard','/services']
 }
